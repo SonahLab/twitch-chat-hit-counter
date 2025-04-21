0a44d309132280cf8a8482bc039d3ad584ee2add
SonahBoi <sonahs.lab@gmail.com>
2025-04-21 13:34:10 -0700

    Module 1 Exercise 2

--- src/main/java/com/sonahlab/twitch_chat_hit_counter_course/rest/ApplicationRestController.java
+++ src/main/java/com/sonahlab/twitch_chat_hit_counter_course/rest/ApplicationRestController.java
@@ -1,5 +1,6 @@
 package com.sonahlab.twitch_chat_hit_counter_course.rest;
 
+import com.sonahlab.twitch_chat_hit_counter_course.module.Module1;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
@@ -24,6 +25,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationRestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationRestController.class);
 
+    private Module1 module1;
+
+    public ApplicationRestController(Module1 module1) {
+        this.module1 = module1;
+    }
+
     /**
     * HTTP GET request handler for endpoint /api/hello. Endpoint that will receive an input name and
     * return a custom greeting.
@@ -35,7 +42,7 @@ public class ApplicationRestController {
     @Operation(summary = "Say Hello", description = "Returns a greeting message")
     public String sayHello(@RequestParam String name) {
         /**
-         * TODO: Implement as part of Module 1
+         * TODO: Implement as part of Module 1 Exercise 1.
          * */
         return null;
     }

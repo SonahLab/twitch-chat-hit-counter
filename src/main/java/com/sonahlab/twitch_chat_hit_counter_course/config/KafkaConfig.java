956b22eb2fd1a455b811c396dc45a4bf3af81ef3
SonahBoi <sonahs.lab@gmail.com>
2025-11-10 21:48:48 -0800

    Module 2 Exercise 1

--- src/main/java/com/sonahlab/twitch_chat_hit_counter_course/config/KafkaConfig.java
+++ src/main/java/com/sonahlab/twitch_chat_hit_counter_course/config/KafkaConfig.java
@@ -1,6 +1,15 @@
 package com.sonahlab.twitch_chat_hit_counter_course.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Config class for all of our application's beans. The beans defined here are singletons that we
@@ -13,4 +22,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KafkaConfig {
     // TODO: Add Kafka configs here
+    @Bean
+    public ProducerFactory<String, byte[]> producerFactory(
+            @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers,
+            @Value("${spring.kafka.producer.key-serializer}") String keySerializer,
+            @Value("${spring.kafka.producer.value-serializer}") String valueSerializer
+    ) {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, byte[]> kafkaTemplate(ProducerFactory<String, byte[]> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 }
